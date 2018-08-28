a9eb7a2e24fa95e5214c7f9b5a6f108fc9e31686
SergeA <ronnieo@gmail.com>
2018-08-28 18:59:18 -0400

    Exercise completed

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,40 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+/**
+ * Created by angelov on 8/28/2018.
+ */
+
+public class Emojifier {
+
+    public static final String TAG = Emojifier.class.getSimpleName();
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        Log.d(TAG, "Number of faces detected " + faces.size());
+
+        if (faces.size() == 0) {
+            Toast.makeText(context, "No faces detected", Toast.LENGTH_SHORT).show();
+        }
+
+        detector.release();
+
+    }
+}
