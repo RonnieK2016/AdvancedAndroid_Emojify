ac2f7019df84088f3935877ff34ff1cc72134125
SergeA <ronnieo@gmail.com>
2018-08-28 22:04:33 -0400

    Exercise completed

---
+++ app/src/main/java/com/example/android/emojify/Emoji.java
@@ -0,0 +1,16 @@
+package com.example.android.emojify;
+
+/**
+ * Created by angelov on 8/28/2018.
+ */
+
+public enum Emoji {
+    LEFT_EYE_WINK,
+    RIGHT_EYE_WINK,
+    EYES_CLOSED_SMILE,
+    SMILE,
+    LEFT_EYE_WINK_FROWN,
+    RIGHT_EYE_WINK_FROWN,
+    EYES_CLOSED_FROWN,
+    FROWN
+}
